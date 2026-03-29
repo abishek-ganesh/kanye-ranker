fb845c1461bb7d1e8c8c005392836cad9be263cd
Abishek Ganesh <dangershak@gmail.com>
2026-03-28 22:51:35 -0700

    refactor: consolidate quote system into single rotating section
    - Merge bottom quotes into top quote section (21 total quotes)
    - Remove hardcoded quote from HTML so JS picks a random one on load
    - Cycle quotes every 15 seconds with fade effect
    - Fix smart quote characters that broke JS parsing
    - Remove bottom-quote CSS and HTML

--- js/kanye-quotes.js
+++ js/kanye-quotes.js
@@ -7,19 +7,19 @@
             type: "humorous"
         },
         {
-            quote: "I am the number one human being in music. That means any person that’s living or breathing is number two.",
+            quote: "I am the number one human being in music. That means any person that's living or breathing is number two.",
             type: "confident"
         },
         {
-            quote: "When I think of competition it’s like I try to create against the past. I think about Michelangelo and Picasso, you know, the pyramids.",
+            quote: "When I think of competition it's like I try to create against the past. I think about Michelangelo and Picasso, you know, the pyramids.",
             type: "inspirational"
         },
         {
-            quote: "50 is Eminem’s favorite rapper. I’m my favorite rapper.",
+            quote: "50 is Eminem's favorite rapper. I'm my favorite rapper.",
             type: "humorous"
         },
         {
-            quote: "I hate when I’m on a plane and I wake up with a water bottle next to me like oh great now I gotta be responsible for this water bottle.",
+            quote: "I hate when I'm on a plane and I wake up with a water bottle next to me like oh great now I gotta be responsible for this water bottle.",
             type: "philosophical"
         },
         {
@@ -27,11 +27,11 @@
             type: "inspirational"
         },
         {
-            quote: "My music isn’t just music – it’s medicine.",
+            quote: "My music isn't just music – it's medicine.",
             type: "confident"
         },
         {
-            quote: "I don’t even listen to rap. My apartment is too nice to listen to rap in.",
+            quote: "I don't even listen to rap. My apartment is too nice to listen to rap in.",
             type: "humorous"
         },
         {
@@ -39,11 +39,11 @@
             type: "inspirational"
         },
         {
-            quote: "If you’re a Kanye West fan, you’re not a fan of me; you’re a fan of yourself. You will believe in yourself. I’m just the espresso. I’m just the shot in the morning to get you going. Make you believe that you can overcome that situation that you’re dealing with all the time.",
+            quote: "If you're a Kanye West fan, you're not a fan of me; you're a fan of yourself. You will believe in yourself. I'm just the espresso. I'm just the shot in the morning to get you going. Make you believe that you can overcome that situation that you're dealing with all the time.",
             type: "inspirational"
         },
         {
-            quote: "Look at Gaga she’s the creative director of Polaroid. I like some of the Gaga songs but what the f*** does she know about cameras?",
+            quote: "Look at Gaga she's the creative director of Polaroid. I like some of the Gaga songs but what the f*** does she know about cameras?",
             type: "confident"
         },
         {
@@ -51,19 +51,19 @@
             type: "confident"
         },
         {
-            quote: "Name one genius that ain’t crazy.",
+            quote: "Name one genius that ain't crazy.",
             type: "philosophical"
         },
         {
-            quote: "Everything I’m not made me everything I am.",
+            quote: "Everything I'm not made me everything I am.",
             type: "inspirational"
         },
         {
-            quote: "Everybody want to know what I’d do if I didn’t win. I guess we’ll never know.",
+            quote: "Everybody want to know what I'd do if I didn't win. I guess we'll never know.",
             type: "confident"
         },
         {
-            quote: "We all self-conscious. I’m just the first to admit it.",
+            quote: "We all self-conscious. I'm just the first to admit it.",
             type: "honest"
         },
         {
@@ -71,7 +71,7 @@
             type: "inspirational"
         },
         {
-            quote: "I’m like a vessel, and God has chosen me to be the voice.",
+            quote: "I'm like a vessel, and God has chosen me to be the voice.",
             type: "spiritual"
         },
         {
@@ -79,7 +79,7 @@
             type: "honest"
         },
         {
-            quote: "I no longer have a manager. I can’t be managed.",
+            quote: "I no longer have a manager. I can't be managed.",
             type: "inspirational"
         },
         {
@@ -88,56 +88,43 @@
         }
     ];
 
-    // Function to get a random quote
     function getRandomQuote() {
         return kanyeQuotes[Math.floor(Math.random() * kanyeQuotes.length)];
     }
 
-    // Function to set the main quote
     function setMainQuote() {
-        const mainQuoteElement = document.querySelector(‘.main-quote’);
-
-        if (mainQuoteElement) {
-            const quote = getRandomQuote();
-            mainQuoteElement.textContent = `"${quote.quote}"`;
-            mainQuoteElement.dataset.quoteType = quote.type;
+        var el = document.querySelector('.main-quote');
+        if (el) {
+            var q = getRandomQuote();
+            el.textContent = '"' + q.quote + '"';
+            el.dataset.quoteType = q.type;
         }
     }
 
-    // Function to cycle main quote with fade effect
     function cycleMainQuote() {
-        const mainQuoteElement = document.querySelector(‘.main-quote’);
-
-        if (mainQuoteElement) {
-            mainQuoteElement.style.transition = ‘opacity 0.5s ease’;
-            mainQuoteElement.style.opacity = ‘0’;
+        var el = document.querySelector('.main-quote');
+        if (el) {
+            el.style.transition = 'opacity 0.5s ease';
+            el.style.opacity = '0';
         }
-
-        setTimeout(() => {
+        setTimeout(function() {
             setMainQuote();
-
-            if (mainQuoteElement) {
-                mainQuoteElement.style.opacity = ‘1’;
+            if (el) {
+                el.style.opacity = '1';
             }
         }, 500);
     }
 
-    // Initialize when DOM is ready
     function init() {
-        // Set the main quote on page load
         setMainQuote();
-
-        // Start cycling after a delay
-        setTimeout(() => {
+        setTimeout(function() {
             cycleMainQuote();
-            // Then cycle every 10 seconds
-            setInterval(cycleMainQuote, 10000);
-        }, 5000);
+            setInterval(cycleMainQuote, 15000);
+        }, 15000);
     }
 
-    // Wait for DOM to be ready
-    if (document.readyState === ‘loading’) {
-        document.addEventListener(‘DOMContentLoaded’, init);
+    if (document.readyState === 'loading') {
+        document.addEventListener('DOMContentLoaded', init);
     } else {
         init();
     }
